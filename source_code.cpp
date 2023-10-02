e8ac2aea1f56d159a78fe2359d4d7b9d623e3089
salmanahmed333 <salmanahmed10124@gmail.com>
2023-10-02 19:24:55 +0500

    Adding basic layout of the game

---
+++ source_code.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+void game_menu();
+void select_mode();
+void play(string, int, int);
+
+int main()
+{
+    game_menu();
+    return 0;
+}
+
+void game_menu ()
+{
+    system("COLOR F1");
+    cout<<"************************************** Number Guessing Game Menu **************************************"
+        <<endl<<endl
+        <<"1. Play Game"<<endl<<"2. High Scores"<<endl<<"3. Exit"<<endl
+        <<"Select your Option: ";
+    int choice;
+    cin>>choice;
+    switch (choice)
+    {
+    case 1:
+        select_mode();
+    case 2:
+        system("CLS");
+        cout<<"No High-scores!"<<endl;
+        game_menu();
+    case 3:
+        exit(0);
+    default:
+        system("CLS");
+        cout<<"Invalid choice! Try again."<<endl;
+        game_menu();
+    }
+}
+
+void select_mode()
+{
+    system("CLS");
+    system("COLOR 10");
+    cout<<"Choose Your Game Mode"<<endl
+        <<"1. Easy"<<endl<<"2. Medium"<<endl<<"3. Hard"<<endl<<"4. Custom"<<endl<<"5. Back"<<endl
+        <<"Enter your selection number: ";
+    int selection;
+    cin>>selection;
+    switch (selection)
+    {
+    case 1:
+        play("Easy", 1, 20);
+    case 2:
+        play("Medium", 1, 50);
+    case 3:
+        play("Hard", 1, 100);
+    case 4:
+        system("CLS");
+        int num1, num2;
+        cout<<"Enter starting range: ";
+        cin>>num1;
+        cout<<"Enter ending range: ";
+        cin>>num2;
+        if (num1 < num2)
+            play("Custom", num1, num2);
+        else
+            select_mode();
+    case 5:
+        game_menu();
+    default:
+        game_menu();
+    }
+}
+
+void play (string game_mode, int range_start, int range_end)
+{
+    system("CLS");
+    system("COLOR 49");
+    unsigned int number_of_turns = 0;
+    int random_number = 6;
+    int chosen_number;
+    cout<<game_mode<<endl;
+    cout<<"The number is between "<<range_start<<" and "<<range_end<<endl;
+    do
+    {
+        number_of_turns++;
+        cout<<"Turn #"<<number_of_turns<<": Enter your guess (or enter -1 to quit game): ";
+        cin>>chosen_number;
+        if (chosen_number == -1)
+            break;
+        else if (chosen_number < random_number)
+            cout<<"Your guess is lower than the actual number!"<<endl;
+        else if (chosen_number > random_number)
+            cout<<"Your guess is higher than the actual number!"<<endl;
+    }
+    while (chosen_number != random_number);
+    if (chosen_number == random_number)
+    {
+        cout<<"Congratulations! You won by guessing the correct number."<<endl
+            <<"The number was "<<random_number<<endl
+            <<"Total turns taken: "<<number_of_turns<<endl
+            <<"Do you want to play another game? (y/n): ";
+        char opt;
+        cin>>opt;
+        switch (opt)
+        {
+        case 'y':
+            system("CLS");
+            play(game_mode, range_start, range_end);
+        case 'n':
+            system("CLS");
+            game_menu();
+        default:
+            system("CLS");
+            cout<<"Invalid choice! Returning to main menu..."<<endl;
+            game_menu();
+        }
+    }
+    else
+    {
+        system("CLS");
+        cout<<"The number was "<<random_number<<endl;
+        game_menu();
+    }
+}
